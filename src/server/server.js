5c1478be7b93ba60310c3813902647dde1bf8ac3
Thibault Friedrich <tf@elqui.fr>
2017-04-23 17:40:05 +0200

    correction path for production

--- src/server/server.js
+++ src/server/server.js
@@ -22,7 +22,7 @@ const app = express()
 const server = http.Server(app)
 const io = socketIo(server)
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../../src/public')))
 
 // app.get('/', (req, res) => {
 //   res.send('Hello world')
